15d94f16f8cc021ec90f2fb53ac5be950eac7119
odeardika <dirfanardika@gmail.com>
2023-10-20 21:04:13 +0800

    trying cron

--- src/app/api/get-article/route.ts
+++ src/app/api/get-article/route.ts
@@ -1,6 +1,5 @@
 import { getHackerNewsStory } from "@/lib/getHackerNewsStory";
-import { NextResponse } from "next/server";
 
 export async function GET(req: Request, res: Response) {
-    return getHackerNewsStory()
+    getHackerNewsStory()
 }
